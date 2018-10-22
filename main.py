cd20b609c067766359503531764230f26a8891c4
Lukasz Cieply <hurricane.pl.96@gmail.com>
2018-10-22 15:29:53 +0200

    main enclosed in function and more comments

--- main.py
+++ main.py
@@ -17,23 +17,41 @@
 #   tytul VARCHAR(100),
 #   PRIMARY KEY(lp));
 
-table = "wypozyczenia"
-
-data = list()
-with open("data/"+table+".txt") as f_studenci:
-    for line in f_studenci:
-        line = line.strip()
-        data_column = line.split("\t")
-        data.append(data_column)
-print(data)
-
 from wrapper_style_connector import insert_list, delete_data_of_table
-
-delete_data_of_table(table)
-insert_list(table, data)
-
 from class_style_mysql_connector import DatabaseHandler
 
-db = DatabaseHandler()
-db.delete_data_of_table(table)
-db.insert_list(table, data)
+
+def main():
+    """
+    Import some data from file and INSERT it into mysql database.
+
+    There are 2 styles of connectors used:
+
+    *    :py:class:`class_style_mysql_connector`
+    *    :py:class:`wrapper_style_connector.mysql_decorator`
+
+    :return:
+    """
+    table = "wypozyczenia"
+
+    data = list()
+    with open("data/"+table+".txt") as f_studenci:
+        for line in f_studenci:
+            line = line.strip()
+            data_column = line.split("\t")
+            data.append(data_column)
+    print(data)
+
+
+    delete_data_of_table(table)
+    insert_list(table, data)
+
+
+    db = DatabaseHandler()
+    db.delete_data_of_table(table)
+    db.insert_list(table, data)
+    del db
+
+
+if __name__ == "__main__":
+    main()
